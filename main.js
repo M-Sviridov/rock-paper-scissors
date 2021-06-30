07a0b248804387d67492b10be85ef9a5641a16d5
Mathieu Sviridov <msviridov@pm.me>
2021-06-30 11:58:52 +1000

    Calling the function game() until one of the players has reached five points and asking for reset once reached

--- main.js
+++ main.js
@@ -3,24 +3,46 @@ let playerPoints = 0;
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+const playContainer = document.querySelector('.play-container');
+const playContent = document.createElement('h4');
+const playResult = document.createElement('h4');
+const resetButtonContainer = document.querySelector('.reset-button-container');
+const resetButton = document.createElement('button');
+
+rock.addEventListener('click', () => {
+    if (playerPoints < 5 && computerPoints < 5) {
+        game(playerSelection = rock.getAttribute('value'))
+    } else {
+        return;
+    }
+});
+
+paper.addEventListener('click', () => {
+    if (playerPoints < 5 && computerPoints < 5) {
+        game(playerSelection = paper.getAttribute('value'))
+    } else {
+        return;
+    }
+});
+
+scissors.addEventListener('click', () => {
+    if (playerPoints < 5 && computerPoints < 5) {
+        game(playerSelection = scissors.getAttribute('value'))
+    } else {
+        return;
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    window.location.reload()
+});
 
 function computerPlay() {
     let moves = ['rock', 'paper', 'scissors'];
     return moves[Math.floor(Math.random() * moves.length)];
 }
-
-rock.addEventListener('click', () => {
-    game(playerSelection = rock.getAttribute('value'))
-});
-
-paper.addEventListener('click', () => {
-    game(playerSelection = paper.getAttribute('value'))
-});
-
-scissors.addEventListener('click', () => {
-    game(playerSelection = scissors.getAttribute('value'))
-});
-
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -38,22 +60,46 @@ function playRound(playerSelection, computerSelection) {
 function game(playerSelection) {
     const computerSelection = computerPlay();
 
+    playContent.textContent = 'Current play: '
+        + playerSelection + ' vs ' + computerSelection;
+    playContent.id = 'play-content';
+    playContainer.appendChild(playContent);
+
     if (playRound(playerSelection, computerSelection) === 'player') {
         playerPoints++;
-        console.log('The Player wins the round.')
+        playerScore.textContent = playerPoints;
+        playResult.textContent = 'The player wins the round.';
+        playResult.id = 'play-result';
+        playContainer.appendChild(playResult);
     } else if (playRound(playerSelection, computerSelection) === 'computer') {
         computerPoints++;
-        console.log('The Computer wins the round.')
+        computerScore.textContent = computerPoints;
+        playResult.textContent = 'The computer wins the round.'
+        playResult.id = 'play-result';
+        playContainer.appendChild(playResult);
     } else if (playRound(playerSelection, computerSelection) === 'tie') {
-        console.log('It is a tie!')
+        playResult.textContent = 'It is a tie!';
+        playResult.id = 'play-result';
+        playContainer.appendChild(playResult);
     }
 
     if (computerPoints === 5) {
-        console.log('The Computer has won the game.')
+        playResult.textContent = `The computer has won the game.
+            Please use the reset button.`;
+        playResult.id = 'game-result';
+        playContainer.appendChild(playResult);
+        resetButton.textContent = 'RESET';
+        resetButton.id = 'reset-button';
+        resetButtonContainer.appendChild(resetButton);
+        return;
     } else if (playerPoints === 5) {
-        console.log('The Player has won the game.')
+        playResult.textContent = `The player has won the game.
+            Please use the reset button.`;
+        playResult.id = 'game-result';
+        playContainer.appendChild(playResult);
+        resetButton.textContent = 'RESET';
+        resetButton.id = 'reset-button';
+        resetButtonContainer.appendChild(resetButton);
+        return;
     }
-
-    console.log(playerPoints, computerPoints)
-
 }
